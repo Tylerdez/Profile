f7adeb037fe1d3d7e0733f0978227bda7167f5ca
Tylerdez <tyler.dezern@gmail.com>
2020-05-14 17:55:48 -0400

    add JS

---
+++ code.js
@@ -0,0 +1,17 @@
+var colorChange = document.getElementById("change");
+
+colorChange.addEventListener("mouseover", function () {
+  colorChange.style = "color:green";
+});
+
+var colorChang = document.getElementById("change1");
+
+colorChang.addEventListener("mouseover", function () {
+  colorChang.style = "color:green";
+});
+
+var colorChan = document.getElementById("change2");
+
+colorChan.addEventListener("mouseover", function () {
+  colorChan.style = "color:green";
+});
